729fdc4273f34fb87f93eff7582713595b9cd796
RajaS <RajaS@INBEN10096L.kongsberg.master.int>
2016-11-10 22:34:38 +0530

    #SP-1 Fixed EmPower Testing Framework

---
+++ src/test/java/com/EmPower/TestScripts/Login.java
@@ -0,0 +1,27 @@
+package com.EmPower.TestScripts;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.Test;
+
+public class Login{
+	
+	static WebDriver driver;
+
+	@Test
+	public void loginApp() throws Exception {
+
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("chrome.switches","--disable-extensions");
+		System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir")+"/browser-drivers/chromedriver.exe");
+		driver = new ChromeDriver(options);
+		
+		driver.manage().window().maximize();
+
+		Thread.sleep(1000);
+		
+		driver.get("https://wfms.kongsberg.master.int");
+
+    }   
+}
